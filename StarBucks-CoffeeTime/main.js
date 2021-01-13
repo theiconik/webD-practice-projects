f085ddbd02419b38a3046ce1a902382df6d07cf4
theiconik <kumarujjawal51@gmail.com>
2021-01-13 21:26:46 +0530

    Added responsive design code

--- StarBucks-CoffeeTime/main.js
+++ StarBucks-CoffeeTime/main.js
@@ -6,4 +6,11 @@ function changeCircleColor(color){
    const circle = document.querySelector('.circle');
    circle.style.background= color;
    console.log(color);
+} 
+
+function toggleMenu(){
+   var menuToggle = document.querySelector(".toggle");
+   var nav= document.querySelector('.navigation');
+   menuToggle.classList.toggle('active');
+   nav.classList.toggle('active');
 }
