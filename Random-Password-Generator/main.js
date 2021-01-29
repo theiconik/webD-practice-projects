bf61bff359749ab1ca9d447b2cb61a9a24cc8b8e
theiconik <kumarujjawal51@gmail.com>
2021-01-30 00:08:50 +0530

    CopyPassword function created

--- Random-Password-Generator/main.js
+++ Random-Password-Generator/main.js
@@ -1,5 +1,6 @@
 var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*_?:";
-var op= document.getElementById("password");
+var op = document.getElementById("password");
+var alertBox = document.querySelector(".alertBox");
 
 function generatePwd() {
   var pwdl = 16;
@@ -7,9 +8,28 @@ function generatePwd() {
   for (var i = 0; i < pwdl; i++) {
     password = password + chars[Math.floor(Math.random() * chars.length)];
   }
-  op.innerHTML= password;
+  op.innerHTML = password;
+  alertBox.innerHTML = "Copied!";
 }
 
-function reset(){
-   op.innerHTML = "";
+function reset() {
+  op.innerHTML = "";
+}
+
+function copyPassword(containerid) {
+  if (document.selection) {
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select().createTextRange();
+    document.execCommand("copy");
+  } else if (window.getSelection) {
+    var range = document.createRange();
+    range.selectNode(document.getElementById(containerid));
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    alertBox.classList.toggle("active");
+    setTimeout(function () {
+      alertBox.classList.toggle("active");
+    }, 1000);
+  }
 }
