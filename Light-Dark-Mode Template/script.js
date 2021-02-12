27e898b709ef47be303e4c7b49113629f77d85a5
theiconik <kumarujjawal51@gmail.com>
2021-02-12 13:07:47 +0530

    Local Storage feature added

--- Light-Dark-Mode Template/script.js
+++ Light-Dark-Mode Template/script.js
@@ -36,12 +36,26 @@ function switchTheme(event) {
    //console.log(event.target.checked);
    if(event.target.checked) {
       document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
       darkMode();
    } else {
       document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
       lightMode();
    }
 }
 
 //Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+//Check Local Storage for theme
+const currentTheme = localStorage.getItem('theme');
+//console.log(currentTheme);
+if(currentTheme) {
+   document.documentElement.setAttribute('data-theme', currentTheme);
+
+   if(currentTheme === 'dark') {
+      darkMode();
+      toggleSwitch.checked = true;
+   }
+}
